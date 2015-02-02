b992285b93ca99fdf5ef31e9d919b6b142f5ca51
mayav <mayav@yahoo-inc.com git config --global user.name mayavenkaraman git config --global user.email mayav@yahoo-inc.com>
2015-02-01 23:50:57 -0800

    scroll
    trying to get scroll to work

---
+++ fb-2-feb1/photoViewController.swift
@@ -0,0 +1,39 @@
+//
+//  photoViewController.swift
+//  fb-2-feb1
+//
+//  Created by Maya Venkatraman on 2/1/15.
+//  Copyright (c) 2015 Maya Venkatraman. All rights reserved.
+//
+
+import UIKit
+
+class photoViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+scrollView.contentSize = imageView.image!.size
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
