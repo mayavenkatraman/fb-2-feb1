80940afcc17b95c998d1db7bec9d4cc83a07d2f4
mayav <mayav@yahoo-inc.com git config --global user.name mayavenkaraman git config --global user.email mayav@yahoo-inc.com>
2015-02-01 16:27:08 -0800

    Facebook Exercise
    Facebook Exercise - first commit

---
+++ fb-2-feb1/RequestsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RequestsViewController.swift
+//  fb-2-feb1
+//
+//  Created by Maya Venkatraman on 2/1/15.
+//  Copyright (c) 2015 Maya Venkatraman. All rights reserved.
+//
+
+import UIKit
+
+class RequestsViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+       
+    }
+   
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ fb-2-feb1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fb-2-feb1
+//
+//  Created by Maya Venkatraman on 2/1/15.
+//  Copyright (c) 2015 Maya Venkatraman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
